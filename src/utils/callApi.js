d57437d10319f3ba21223e5b29196ac2ef654de6
Tran Phuong <tranphuongdev31397@gmail.com>
2021-10-23 17:48:41 +0700

    fix link login

--- src/utils/callApi.js
+++ src/utils/callApi.js
@@ -1,13 +1,19 @@
 import axios from "axios";
-import { BASE_URL } from "../settings/apiConfig";
+import { BASE_URL, CYBERSOFT_TOKEN } from "../settings/apiConfig";
 
-export const callApi = (endpoint, method = 'GET', data = null, token = null) => {
-    return axios({
-        url: `${BASE_URL}/${endpoint}`,
-        method,
-        data,
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    })
-}
+export const callApi = (
+  endpoint,
+  method = "GET",
+  data = null,
+  token = null
+) => {
+  return axios({
+    url: `${BASE_URL}/${endpoint}`,
+    method,
+    data,
+    headers: {
+      TokenCybersoft: `${CYBERSOFT_TOKEN}`,
+      Authorization: `${token}`,
+    },
+  });
+};
